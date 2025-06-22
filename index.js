c684c963e3a8f2022ad0ff6f16aecb0ef614d500
SanjuHashintha <sanjuhashintha@gmail.com>
2025-06-22 19:37:17 +0530

    remove upload folder

--- index.js
+++ index.js
@@ -10,36 +10,22 @@ import notesRouter from "./routes/notes.js";
 import categoryRouter from "./routes/categories.js";
 import tagsRouter from "./routes/tags.js";
 
-console.log("starting server console log");
-
 dotenv.config();
 connectDB();
 
-console.log("test1");
-
 const app = express();
-app.get("/", (req, res) => {
-  res.send("starting server");
-});
-console.log("test2");
 app.use(express.json());
-console.log("test3");
+const PORT = process.env.PORT || 3000;
 app.use(cors());
-console.log("test4");
-
-app.use("/uploads", express.static("uploads"));
-console.log("test5");
 
 app.use("/api", authRouter);
 app.use("/api", usersRouter);
 app.use("/api", notesRouter);
 app.use("/api", categoryRouter);
 app.use("/api", tagsRouter);
-console.log("test6");
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express with vercel!");
+  res.send("Hello from Express with sdsdsssss!");
 });
-console.log("test7");
 
 export default serverless(app);
